57311f633b8ac20630c1fae3c474220c5481ec18
mrchi <mr.chilli.the.hot@gmail.com>
2018-05-17 21:07:05 +0200

    simple lotto application

---
+++ src/main/java/com/example/lotto/demo/controllers/LottoController.java
@@ -0,0 +1,25 @@
+package com.example.lotto.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+@Controller
+public class LottoController {
+
+    @GetMapping("/")
+    public String getHello(ModelMap modelMap){
+        Set<Integer> set = new TreeSet<>();
+        Random random = new Random();
+        while (set.size() != 6){
+               set.add(random.nextInt(49)+1 );
+        }
+
+        modelMap.put("numer",set);
+        return "Hello";
+    }
+}
